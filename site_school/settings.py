aae8afb0b17a942fd7946ab57bf0a235155aba21
rosdfots20 <iam@email.com>
2021-11-13 18:04:15 -0600

    config pwa

--- site_school/settings.py
+++ site_school/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = '2zn+_kj3!nbfo&3g5rqh&%!g2w0jgse0)f*-0)phv9s&)@*d@y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'site_school.urls'
@@ -113,30 +113,84 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-PWA_APP_NAME = "UNIVERSIDAD"
-PWA_APP_DESCRIPTION = "Pagina de Tecnologia"
+PWA_APP_NAME = "Universidad"
+PWA_APP_DESCRIPTION = "Universidad de los Angeles"
 PWA_APP_THEME_COLOR = "#3477f5"
 PWA_APP_BACKGROUND_COLOR = "#6699f7"
 PWA_APP_ICONS = [
     {
-        "src": "/static/core/img/iconoprueba.ico",
-        "sizes": "160x160" 
+        "src": "/static/core/img/icons/iconx72.png",
+        "sizes": "72x72" 
     },
     {
-        "src": "/static/core/img/iconoprueba144x.png",
+        "src": "/static/core/img/icons/iconx96.png",
+        "sizes": "96x96" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx128.png",
+        "sizes": "128x128" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx144.png",
         "sizes": "144x144" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx152.png",
+        "sizes": "152x152" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx192.png",
+        "sizes": "192x192" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx384.png",
+        "sizes": "384x384" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx512.png",
+        "sizes": "512x512" 
     }
 ]
 
 PWA_APP_ICONS_APPLE = [
     {
-        "src": "/static/core/img/iconoprueba.ico",
-        "sizes": "160x160" 
+        "src": "/static/core/img/icons/iconx72.png",
+        "sizes": "72x72" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx96.png",
+        "sizes": "96x96" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx128.png",
+        "sizes": "128x128" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx144.png",
+        "sizes": "144x144" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx152.png",
+        "sizes": "152x152" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx192.png",
+        "sizes": "192x192" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx384.png",
+        "sizes": "384x384" 
+    },
+    {
+        "src": "/static/core/img/icons/iconx512.png",
+        "sizes": "512x512" 
     }
 ]
+
+PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, "serviceworker.js")
